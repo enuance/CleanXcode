a3cd69504a975090fc618dc85b8f32b11f6b6873
Stephen Martinez <enharmonicnuance@gmail.com>
2020-01-20 12:17:50 -0800

    [CXFeature-001][Consler] Added the Consler and SPMUtilities packages to the project

--- Package.swift
+++ Package.swift
@@ -6,16 +6,19 @@ import PackageDescription
 let package = Package(
     name: "CleanXcode",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0")
+        // 📁 John Sundell's Files Package is great for easy file reading/writing/moving/etc.
+        .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
+        // 🧰 SPMUtilities for CLI Argument Parsing.
+        .package(url: "https://github.com/apple/swift-package-manager", from: "0.5.0"),
+        // Consler for Styled outputs to the Console
+        .package(url: "https://github.com/enuance/consler", from: "0.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CleanXcode",
-            dependencies: ["Files"]),
+            dependencies: ["Files", "SPMUtility", "Consler"]),
         .testTarget(
             name: "CleanXcodeTests",
             dependencies: ["CleanXcode"]),
