6973cbd5a57b14c0c48c9d9059fffd4acb6204a6
Stephen Martinez <enharmonicnuance@gmail.com>
2020-01-20 12:41:22 -0800

    [CXFeature-001] Updated the installer, read me, and package manafest for the correct executable name.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "CleanXcode",
+    name: "cleanXcode",
+    platforms: [.macOS(.v10_15)],
     dependencies: [
         // 📁 John Sundell's Files Package is great for easy file reading/writing/moving/etc.
         .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
@@ -17,10 +17,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "CleanXcode",
-            dependencies: ["Files", "SPMUtility", "Consler"]),
+            name: "cleanXcode",
+            dependencies: ["Files", "SPMUtility", "Consler"],
+            path: "Sources/CleanXcode"),
         .testTarget(
             name: "CleanXcodeTests",
-            dependencies: ["CleanXcode"]),
+            dependencies: ["cleanXcode"]),
     ]
 )
